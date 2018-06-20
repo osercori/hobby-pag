14c3926c9196f22e18a4480a188f4fa57045e0ff
Oswaldo Colocho <osercori@gmail.com>
2018-06-20 18:21:57 +0000

    Add JavaScript and changed index

---
+++ javascript.js
@@ -0,0 +1,72 @@
+/*
+
+//console.log("Something");
+//https://websitebeaver.com/set-up-localhost-on-macos-high-sierra-apache-mysql-and-php-7-with-sslhttps
+var days=["Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday","SuperDay"]; //square brackets
+
+var length =days.length;
+var counter=0;
+while(counter<length){
+    console.log(days[counter]);
+    counter=counter+1;
+}
+console.log("____________________");
+var favoriteFruits=["Kiwi","Apple","Banana","Dragonfruit","Strawberry","Blueberries"];
+
+console.log("My favorite Fruits are:");
+//Create counter
+var counter2=0;
+    //Compare counter
+while(counter2<10){
+    //Do something with counter
+    counter2=counter2+1;
+}
+//  create counter  | Compare counter   |Do something whit counter
+for(var counter3=0  ;counter3<10        ;counter3=counter3+1)
+{
+    
+}
+
+var exampleArray = [1,2,3,4];
+
+var exampleObject ={
+    "firstNumber": 1,
+    "secondNumber": 2,
+    "thirdNumber" :3
+};
+
+console.log(exampleObject["secondNumber"]);
+
+console.log(exampleObject.thirdNumber);
+*/
+
+/*
+    var user ={
+        "username":"Oswaldo",
+        "password":"123456",
+        "accountId":"0000001",
+        "userAge":"24",
+        "isActive":true
+
+var honda ={
+    "car-serial":239838947239,
+    "owner":"Oswaldo"
+};
+var toyota ={
+    "car-serial":736491734053,
+    "owner":"no one"
+};
+
+var cars=[honda,toyota];
+console.log("The owner is :"+honda.owner);
+console.log("The serial is: "+honda["car-serial"]);
+console.log("The serial is: "+cars[1]["car-serial"]);    };
+*/
+
+function myFirstFunction(){
+    console.log("Hi there");
+    console.log("My name is: ");
+    console.log("Oswaldo");
+}
+
+myFirstFunction();
